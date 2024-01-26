61fb071ba08ff3b10e713a244c89cfaff553ad87
George Morales <133523465+FaRm3rKing@users.noreply.github.com>
2024-01-26 22:44:03 +0800

    Update +server.ts

--- src/routes/signout/+server.ts
+++ src/routes/signout/+server.ts
@@ -4,6 +4,5 @@ import { redirect } from "@sveltejs/kit";
 export function GET ({ cookies }) {
 		cookies.delete("id_token")
 		cookies.delete("refresh_token")
-		// delete event.locals.user
 		throw redirect(307,getSignoutUrl())
 }
